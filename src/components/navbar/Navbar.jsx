2979195c3fb87efd90591ba4866d272cad613668
Tarek994 <ghali.tarek.123@hotmail.com>
2023-01-28 20:05:00 +0200

    style(navbar):add the style of the right side of the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,19 @@
-import Navbar from "../navbar/Navbar.css";
-import {BsTelephoneFill} from "react-icon"
+import "../navbar/Navbar.css";
+import {BsTelephoneFill} from "react-icons/bs";
+import logo from "../../assets/logo.svg"
 
 const navbar = () => {
   return (
     <div className="navbar col-12" >
-        <div className="main-nav col-10">
+        <div className="main-nav col-12">
             <div className="left-side col-6">
-                <h3 className="logo"> tenant angel</h3>
+                <h3 className="logo"><img src={logo} alt=""/></h3>
                 <p>The Tenant Protection<br/> Club</p>
             </div>
 
             <div className="right-side col-6">
-                <h3 className="contact"> 030 5557 1887</h3>
-                <p>Mon to Fri, 8 a.m. to 6.30 p.m. and Sat, 11 a.m. to 3 p.m</p>
+                <h3 className="contact"><span><BsTelephoneFill className="smallicon"/></span> 030 5557 1887</h3>
+                <p className="address">Mon to Fri, 8 a.m. to 6.30 p.m. and Sat, 11 a.m. to 3 p.m</p>
             </div>
         </div>
     </div>
